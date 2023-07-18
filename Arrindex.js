6e5d87ce0d8aa545cc6fe4056daa74b31295b7f5
Anwar <ambhati54@gmail.com>
2023-07-18 17:10:16 +0530

    array First day Assignment

---
+++ Arrindex.js
@@ -0,0 +1,74 @@
+// 1. Array numbers ka. Loop chlana hai. Agar number odd hai to ODD print hai krna 
+//  hai otherwise EVEN print krna hia
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+// for(i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         console.log("Number is even");
+//     }
+//     else{
+//         console.log("Number is odd");
+//     }
+// }
+
+// 2. Array numbers ka. Sare numbers ko add krke print krna hai 
+
+// let arr = [1,2,3,45,5,5,6,4,3,66,55]
+// let sum = 0
+// for(i = 0; i < arr.length; i++){
+//     sum = sum + arr[i]
+// }
+// console.log(sum);
+
+// 3. Array numbers ka. Sare Odd numbers ko add krke print krna hai 
+
+// let arr = [1,2,3,4,5,6,7,8,44,33,22,11]
+// let sumOfOdd = 0
+// for(i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 === 1){
+//         sumOfOdd = sumOfOdd + arr[i]
+//     }
+// }
+// console.log(sumOfOdd);
+
+// 4. Array numbers ka. Sare Even numbers ko add krke print krna hai 
+
+
+// let arr = [1,2,3,4,5,6,7,8,44,33,22,11]
+// let sumOfEven = 0
+// for(i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         sumOfEven = sumOfEven + arr[i]
+//     }
+// }
+// console.log(sumOfEven);
+
+
+// Array me kitne numbers aise hai jo 100 se bde hai 
+// let arr = [1,2,333,121,132,143,153,545,111,22,332,33,22,77]
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 100){
+//         console.log(arr[i]);
+//     }
+// }
+
+// 10. Array me kitne numbers aise hai jo 20 se bde or Even hai 
+
+// let arr = [2,3,4,5,11,22,43,32,21,33,54,65,44,66,55,56]
+// for(i = 0; i < arr.length; i++){
+//     if(arr[i] > 20 && arr[i] % 2 === 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// 7. Array ka phla number print krvana hai 
+
+// let arr = [1,23,4,5,6,78,5,4,3,2]
+// console.log(arr[0]);
+
+// 8. Array ka last number print krvana hai 
+// let arr = [1,2,3,4,5,9,7]
+// console.log(arr[arr.length -1]);
+
+
+
