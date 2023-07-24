d24ab9ce9e03108482427f610dc9c126a6ffc14b
Anwar <ambhati54@gmail.com>
2023-07-24 11:57:07 +0530

    thirdAssign

---
+++ ThirdAssign.js
@@ -0,0 +1,96 @@
+// 1. Find value greater than 20 using find function 
+
+// let arr = [12,22,33,44,3,2]
+// let ans = arr.find((para)=>{
+//     return para > 20
+// })
+// console.log(ans);
+
+// 2. Find string length greater than 10 using find function 
+// let arr = ["anwar","mohammad","vpp_mokalpur_teh_merta city"]
+// let ans = arr.find((para)=>{
+//     return para.length > 10
+// })
+// console.log(ans);
+
+// 3. Filter all numbers less than 100
+// let arr = [111,222,333,22,33,43,21]
+// let ans = arr.filter((para)=>{
+//     return para < 100
+// })
+// console.log(ans);
+
+// 4. Filter all numbers greater than 50 and multiple of 5
+// let arr = [5,55,15,100,90,80,70,54,63,23]
+// let ans = arr.filter((para)=>{
+//    return para > 50 && para % 5 === 0
+// })
+// console.log(ans);
+
+// // 5. Filter all words with length > 5 and contains world 'World' in itself for example
+// //  Hello World,Computer World 
+// let arr = ["hello","computer ","namaste ","jems "]
+// let ans = arr.filter((para)=>{
+//     return para.length > 5
+// },)
+// console.log(ans);
+
+// 6. Calculate sum of all the numbers of array using reduce function 
+// let arr = [1,2,3,4,56,5]
+// let ans = arr.reduce((pre,curr)=>{
+//     // console.log(pre + curr);
+//     return pre + curr
+// },0)
+// console.log(ans);
+
+// 7. Calculate total words with length > 5 in an array using reduce and filter
+//  function. Dono se krna hai alag alag 
+// ??????
+
+// 8. Convert any odd number to even number using map function 
+// let arr = [1,2,3,4,5,6,7,8]
+// let ans = arr.map((para)=>{
+//     if(para % 2 !== 0){
+//         return para + 1
+//     }
+//     else{
+//         return para
+//     }
+// })
+// console.log(ans);
+
+// 9. Convert any even number to odd number using map function 
+// let arr = [1,2,3,4,5,6,7,8,9]
+// let ans = arr.map((para)=>{
+//     if(para % 2 === 0){
+//         return para + 1
+//     }else{
+//         return para
+//     }
+// })
+// console.log(ans);
+
+// 10. Check if every number is multiple of 5 or not in the array using every 
+// function 
+// let arr = [5,55,35,45,33,43]
+// let ans = arr.every((para)=>{
+//     return para % 5 === 0
+// })
+// console.log(ans);
+
+// 11. Check if any number is multiple of 5 and less than 10 using some function
+// let arr = [5,55,45,34,2,35,25,15]
+// let ans = arr.some((para)=>{
+//     return para % 5 === 0 && para < 10
+// })
+// console.log(ans);
+
+
+
+
+
+
+
+
+
+
