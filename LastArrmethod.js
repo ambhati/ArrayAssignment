001ba29500b1b9b9c8c0c9f0947ee40e9dd87eb2
Anwar <ambhati54@gmail.com>
2023-07-27 22:12:43 +0530

    add

---
+++ LastArrmethod.js
@@ -0,0 +1,27 @@
+// let arr = [10, 1, 2, 3, [4, 543,[ 6, [7]]], [8, 9], 10, 11, 12, 13];
+// console.log(arr.flat(3));
+
+// let arr = [2,3,4,5,6,7,5,4]
+// let ans = arr.slice(4)
+// console.log(ans);
+
+// let arr = ["ab","ba","ma","ra"]
+// let ans = arr.splice(1,2)
+// console.log(ans);
+
+// let arr = [2,3,4,5,6,3,4]
+// let ans = arr.slice(-3)
+// let rev = ans.reverse()
+// console.log(rev);
+
+// let arr = [12,33,22,44,43,34,12,21,23,43]
+// let ans = arr.findIndex((value)=>{
+//     return value === 34
+// })
+// console.log(ans);
+// arr.splice(ans,2)
+// console.log(arr);
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+// let ans = arr.splice(3,2)
+// console.log(ans);
